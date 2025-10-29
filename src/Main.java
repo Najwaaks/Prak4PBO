ae85313e8dd01a881ee710ee966ae51a6b1e9d3a
NadiaChelsea72 <nadiachelsea1812@gmail.com>
2025-10-29 12:50:40 +0700

    Update 1 Data

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@ public class Main {
         Pilot P1 = new Pilot("Davin");
         Pilot P2 = new Pilot("Dawim");
         Pilot P3 = new Pilot("Ogaima");
+        Pilot P4 = new Pilot("Davina");
         Maskapai M1 = new Maskapai("Citilink");
         Maskapai M2 = new Maskapai("Garuda Indonesia");
         Maskapai M3 = new Maskapai("Indonesia AirAsia");
+        Maskapai M4 = new Maskapai("Lion Air");
         
         // menampilkan hasil asosiasi
         System.out.println("=== Asosiasi Pilot dan Maskapai ===");
         P1.maskapai(M1); 
         P2.maskapai(M2);
         P3.maskapai(M3);
+        P4.maskapai(M4);
     }
 }
